2c6c3d0687b4056db409d052dd559d734cc9614f
Abdulmuiz Adewale <abdulmuiz.adewale@tolaram.com>
2025-10-20 13:25:03 +0100

    age lesser than zero

---
+++ test_rev.py
@@ -0,0 +1,31 @@
+from rev import APIClient, UserServices, DatabaseClient
+import pytest
+
+#preparing the mocks
+def test_register_user(mocker):
+    """testing UserServices with mocked APIClient and DatabaseClient"""
+    mock_api_client= mocker.Mock(spec= APIClient)
+    mock_db_client= mocker.Mock(spec= DatabaseClient)
+
+    """setting return values for the mocked methods"""
+    mock_api_client.get_user_data.return_value= {
+        'username': 'ibrahim sulaimon',
+        'email': 'ibrahimsulaimon@gmail.com',
+        'age': 1
+    }
+    mock_db_client.save_user.return_value= {
+        'username': 'ibrahim sulaimon',
+        'email': 'ibrahimsulaimon@gmail.com',
+        'age': 1
+    }
+
+    """creating UserServices instance with the mocked clients"""
+    services= UserServices(mock_api_client, mock_db_client)
+    """calling the register_user method"""
+    result= services.register_user('ibrahim sulaimon', 'ibrahimsulaimon@gmail.com', 1)
+
+    #assertations
+    """checking the result and that the mocked methods were called correctly"""
+    assert result == ('IBRAHIM SULAIMON')
+    mock_api_client.get_user_data.assert_called_once_with('ibrahim sulaimon','ibrahimsulaimon@gmail.com', 1)
+    mock_db_client.save_user.assert_called_once_with('ibrahim sulaimon','ibrahimsulaimon@gmail.com', 1)
