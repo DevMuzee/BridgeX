7671f69c7ec8b02efb030f6fda305a7336b45938
Abdulmuiz Adewale <abdulmuiz.adewale@tolaram.com>
2025-10-20 14:16:05 +0100

    test_age>0

--- test_rev.py
+++ test_rev.py
@@ -11,21 +11,21 @@ def test_register_user(mocker):
     mock_api_client.get_user_data.return_value= {
         'username': 'ibrahim sulaimon',
         'email': 'ibrahimsulaimon@gmail.com',
-        'age': 25
+        'age': 0
     }
     mock_db_client.save_user.return_value= {
         'username': 'ibrahim sulaimon',
         'email': 'ibrahimsulaimon@gmail.com',
-        'age': 25
+        'age': 0
     }
 
     """creating UserServices instance with the mocked clients"""
     services= UserServices(mock_api_client, mock_db_client)
     """calling the register_user method"""
-    result= services.register_user('ibrahim sulaimon', 'ibrahimsulaimon@gmail.com', 25)
+    result= services.register_user('ibrahim sulaimon', 'ibrahimsulaimon@gmail.com', 0)
 
     #assertations
     """checking the result and that the mocked methods were called correctly"""
     assert result == ('IBRAHIM SULAIMON')
-    mock_api_client.get_user_data.assert_called_once_with('ibrahim sulaimon','ibrahimsulaimon@gmail.com', 25)
-    mock_db_client.save_user.assert_called_once_with('ibrahim sulaimon','ibrahimsulaimon@gmail.com', 25)
+    mock_api_client.get_user_data.assert_called_once_with('ibrahim sulaimon','ibrahimsulaimon@gmail.com', 0)
+    mock_db_client.save_user.assert_called_once_with('ibrahim sulaimon','ibrahimsulaimon@gmail.com', 0)
